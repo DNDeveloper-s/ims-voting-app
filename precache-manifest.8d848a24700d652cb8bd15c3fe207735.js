7f0621117f64e8ed8d86ee047addafa6dfc79048
dndeveloper <dndeveloper@pop-os.localdomain>
2020-09-01 15:25:49 +0530

    Updates

---
+++ precache-manifest.8d848a24700d652cb8bd15c3fe207735.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85facc4aea0ff912c574b2ea316cf27",
+    "url": "/ims-voting-app/index.html"
+  },
+  {
+    "revision": "a0be01c819c4285ce715",
+    "url": "/ims-voting-app/static/css/main.cf99871d.chunk.css"
+  },
+  {
+    "revision": "175f78213916a05c8b4e",
+    "url": "/ims-voting-app/static/js/2.08b24e68.chunk.js"
+  },
+  {
+    "revision": "9dccab222f778d4f5986790c4cdd8edd",
+    "url": "/ims-voting-app/static/js/2.08b24e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0be01c819c4285ce715",
+    "url": "/ims-voting-app/static/js/main.2c0254db.chunk.js"
+  },
+  {
+    "revision": "f404201cac05aea64be8",
+    "url": "/ims-voting-app/static/js/runtime-main.de997739.js"
+  },
+  {
+    "revision": "2418340e631fcea9cce92dc7373304c0",
+    "url": "/ims-voting-app/static/media/495078-PHE69C-919.2418340e.jpg"
+  }
+]);
