232cf6c92fbaa58089181c0e193566cdd4b1ccdf
dndeveloper <dndeveloper@pop-os.localdomain>
2020-09-02 16:40:32 +0530

    IMS US Homepage link added

--- src/components/Dashboard/AppBar.js
+++ src/components/Dashboard/AppBar.js
@@ -17,7 +17,7 @@ function AppBar(props) {
       <nav className="wrapper">
         <div className="navigation-logo">
           {window.innerWidth > constants.BREAK_POINT ?
-            <img src={logo} alt="Ims Ghaziabad"/> :
+            <a href="https://imsuc.ac.in/" target="_blank"><img src={logo} alt="Ims Ghaziabad"/></a> :
             <div onClick={toggleDrawer(true)} className="material-icons">
               <svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 2.875C0 1.771 0.896 0.875 2 0.875H22.75C23.854 0.875 24.75 1.771 24.75 2.875C24.75 3.979 23.854 4.875 22.75 4.875H2C0.896 4.875 0 3.979 0 2.875ZM22.75 9.375H2C0.896 9.375 0 10.271 0 11.375C0 12.479 0.896 13.375 2 13.375H22.75C23.854 13.375 24.75 12.479 24.75 11.375C24.75 10.271 23.855 9.375 22.75 9.375ZM22.75 17.875H2C0.896 17.875 0 18.771 0 19.875C0 20.979 0.896 21.875 2 21.875H22.75C23.854 21.875 24.75 20.979 24.75 19.875C24.75 18.771 23.855 17.875 22.75 17.875Z" fill="black"/>
