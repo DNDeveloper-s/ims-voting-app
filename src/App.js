8f0c16033dc85a69e04b5316dc22c5dabf591818
dndeveloper <dndeveloper@pop-os.localdomain>
2020-09-04 08:39:30 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/">
+        <Route path={"/"}>
           <Dashboard />
         </Route>
       </Switch>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import AppBar from "./AppBar";
 import ImageCarousel from "./ImageCarousel";
 import TabsHolder from "./TabsHolder";
 
 const Dashboard = (props) => {
+  useEffect(() => {
+    console.log(props);
+  }, []);
   return (
     <div className='ims_dashboard'>
       <AppBar />
